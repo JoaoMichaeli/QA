c41fb91cbe4564db8ecd32796d5c6da2a0f540f1
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-09-10 09:06:47 -0300

    JUNit

---
+++ 2 Semestre/JUnit/Prog1/src/test/java/br/com/fiap/CalculadoraTest.java
@@ -0,0 +1,51 @@
+package br.com.fiap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+    private Calculadora calculadora;
+
+    @BeforeEach
+    public void setUp(){
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void somar(){
+        int a = 5;
+        int b = 4;
+        int result = calculadora.somar(a,b);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void subtrair(){
+        int a = 10;
+        int b = 4;
+        int result = calculadora.subtrair(a,b);
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void multiplicar(){
+        int a = 5;
+        int b = 5;
+        int result = calculadora.multiplicar(a,b);
+
+        assertEquals(25, result);
+    }
+
+    @Test
+    public void dividir(){
+        int a = 10;
+        int b = 2;
+        int result = calculadora.dividir(a,b);
+
+        assertEquals(5, result);
+    }
+
+}
